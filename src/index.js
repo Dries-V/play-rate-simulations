1c57ab951c81a913c84d44c84d5090b8314c4d0e
Dries Vaneechoutte <vaneechoutte.dries@gmail.com>
2021-03-05 09:57:36 +0100

    add timer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const getTimer = require('execution-time')
+
 const { aggregateData } = require("./aggregate-data")
 const { aggregateDataPerRate } = require("./aggregate-data-per-rate")
 const { generateData } = require("./generate-data")
@@ -18,28 +20,39 @@ const main = () => {
   const Ro = input.Io / input.No
   const Rc = input.Ic / input.Nc
   const M = (Ro / Rc) - 1
+
+  const rawTimer = getTimer()
+  rawTimer.start()
   const { average: rawM, CI95: rawCI95 } = simulateRelativeImprovementCI95([{
     optimized: { requests: input.No, playrate: Ro },
     control: { requests: input.Nc, playrate: Rc }
   }])
-  console.log(`Raw:         \t${prettyOutput(M, rawM, rawCI95)}`)
+  const rawTime = rawTimer.stop().time
 
   const Ro1 = input.Io1 / input.No1
   const Rc1 = input.Ic1 / input.Nc1
   const M1 = (Ro1 / Rc1) - 1
+  const approximationTimer = getTimer()
+  approximationTimer.start()
   const { average: approximationM, CI95: approximationCI95 } = simulateRelativeImprovementCI95([{
     optimized: { requests: input.No, playrate: Ro1 },
     control: { requests: input.Nc, playrate: Rc1 }
   }])
+  const approximationTime = approximationTimer.stop().time
 
+  const realisticTimer = getTimer()
+  realisticTimer.start()
   const { average: realisticM, CI95: realisticCI95 } = simulateRelativeImprovementCI95(
     inputPerRate.map(({ Io, No, Ic, Nc }) => ({
       optimized: { requests: No, playrate: Io / No },
       control: { requests: Nc, playrate: Ic / Nc }
     }))
   )
-  console.log(`Realistic:   \t${prettyOutput(M1, realisticM, realisticCI95)}`)
-  console.log(`Approximation:\t${prettyOutput(M1, approximationM, approximationCI95)}`)
+  const realisticTime = realisticTimer.stop().time
+
+  console.log(`Raw:         \t${prettyOutput(M, rawM, rawCI95)} (${Math.floor(rawTime)}ms)`)
+  console.log(`Realistic:   \t${prettyOutput(M1, realisticM, realisticCI95)} (${Math.floor(realisticTime)}ms)`)
+  console.log(`Approximation:\t${prettyOutput(M1, approximationM, approximationCI95)} (${Math.floor(approximationTime)}ms)`)
 }
 
 main()
